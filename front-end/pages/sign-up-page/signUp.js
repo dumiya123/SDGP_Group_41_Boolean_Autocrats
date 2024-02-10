e84944f00c237724f597aca69d7b38e0c82e5413
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2024-02-10 17:02:58 +0530

    Add email and password state and input fields, and a handleSignup function that logs the entered email and password to the console. (Dumindu Gamage)

--- front-end/pages/sign-up-page/signUp.js
+++ front-end/pages/sign-up-page/signUp.js
@@ -1,3 +1,35 @@
 import react,{useState} from "react";
 import { View,Text,TextInput,Button,StyleSheet } from "react-native";
 
+const SignupScreen = () => {
+    const[email,setEmail] = useState('');
+    const[password,setPassword] = useState('');
+
+
+    const handleSignup = () => {
+
+        //In here we are hope to develop the logic for the signup process.
+        //e.g: send data to a backend API 
+        console.log('Email:'.email);
+        console.log('Password:'.password);
+
+        //This is the point to add the API call or Authentication Logic
+
+    };
+
+
+    return(
+        <View style={styles.container}>
+            <Text>Please enter your email and password</Text>
+        </View>
+    )
+    
+  
+
+
+
+
+
+
+
+};
