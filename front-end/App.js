3fe06478150f617176941a628dc95467c6c48d40
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2024-02-11 11:27:12 +0530

    Added necessary imports and created a main App component that returns a container view with flex styling and a SignupScreen component.  And also added some necessary comments for better understanding of the code and better future development.  (Dumindu Gamage).

--- front-end/App.js
+++ front-end/App.js
@@ -1,9 +1,13 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import SignupScreen from './pages/sign-up-page/signUp';
+import * as React from 'react';  //import necessary modules from react and React natve 
+import { View } from 'react-native';    //View is a component of react native that helps in organizing the elements on the screen.
+import SignupScreen from './pages/sign-up-page/signUp';   
 
+
+
+// Main App component
 export default function App() {
   return (
+    // Main container view with flex styling
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <SignupScreen />
     </View>
