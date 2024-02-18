fc9922a11e8dac2d280439026bca7a95bf27559c
Stealth_Black <159097453+StealthBlack99@users.noreply.github.com>
2024-02-19 00:03:59 +0530

    Implemented the OCR function
    implemented the OCR function using an API

---
+++ back-end/OCR-Feature/ocr.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "https://ocr.asprise.com/api/v1/receipt"
+image_path = "D:\SDGP Implementation\SDGP_Group_41_Boolean_Autocrats\\back-end\OCR-Feature\\0.jpg"
+
+res = requests.post(url,
+                    data={
+                        'api_key': 'TEST',
+                        'recognizer': 'auto',
+                        'ref_no': 'oct_python_123'
+                    },
+                    files ={
+                        'file':open(image_path,'rb')
+                    })
+
+with open("response1.json","w") as f:
+    json.dump(json.loads(res.text), f)
