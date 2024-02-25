5ff26fedb3a925ec5ac93da1748588f5555f6b61
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-02-26 03:57:14 +0530

    finalized-re-arrangement

--- front-end/App.js
+++ front-end/App.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import AuthStackNavigator from "./components/AuthStackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
-
+import theme from "./themes/themes";
 
 const App = () => {
-  
   return (
-    <NavigationContainer>
-      <AuthStackNavigator  />
+    <SafeAreaView style={styles.safeArea}>
+      <NavigationContainer theme={theme}>
+        <AuthStackNavigator />
+      </NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
-    </NavigationContainer>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#183D3D'
+  },
+});
 
 export default App;
