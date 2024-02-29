db1a47d2b4c458c737861f99c042f5c79c38c5b4
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2024-02-29 08:46:17 +0530

    Add recent transactions section to Expenses page (Dumindu Gamage).

--- front-end/pages/Profile/Expenses/Expenses.js
+++ front-end/pages/Profile/Expenses/Expenses.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { Text, View } from "react-native";   
+import { Text, View ,ScrollView} from "react-native";   
 import styles from "./ExpensesStyle";
 
 const Expenses = () => {
     return (
         <View>
             <Text></Text>
-            <Text style={styles.header}>   Recent Transactions</Text>
+            <Text style={styles.header}>Recent Transactions</Text>
             <Text></Text>
-            <View>
-                <View>
-                    <Text> </Text>
+            <ScrollView>
+                <View style={styles.box_one}>
+                    <Text></Text>
+                    <Text style={styles.box_one_text}>Foods</Text>
+                    <Text style={styles.box_one_text}>Rs. 1500.00</Text>
+                    <Text></Text>
+                </View>
+                <Text></Text>
+                <View style={styles.box_two}>
+                    <Text></Text>
+                    <Text style={styles.box_two_text}>Medicine</Text>
+                    <Text style={styles.box_two_text}>Rs.3000.00</Text>
+                    <Text></Text>
+
+                </View>
+                <View style={styles.box_three}>
+                    <Text></Text>
+                    <Text style={styles.box_three_text}></Text>
+                    <Text style={styles.box_three_text}></Text>
+
                 </View>
 
-            </View>
+            </ScrollView>
+            
+            
+
             
         </View>
     );
--- front-end/pages/Profile/Expenses/ExpensesStyle.js
+++ front-end/pages/Profile/Expenses/ExpensesStyle.js
@@ -6,7 +6,41 @@ const styles = StyleSheet.create({
     header :
     {
         fontWeight:'bold',
+        justifyContent:'center',
+        marginLeft:95,
         fontSize:20
+    },
+
+    box_one:
+    {
+        backgroundColor:'red',
+        marginLeft:12,
+        borderRadius:60,
+        marginRight:12
+    },
+
+    box_one_text:
+    {
+        marginLeft:30,
+        padding:10
+
+    },
+
+    box_two:
+    {
+        backgroundColor:'red',
+        marginLeft:12,
+        marginRight:12,
+        borderRadius:60
+
+    },
+
+    box_two_text:
+    {
+        marginLeft:30,
+        padding:10
+
+
     }
 
 });
