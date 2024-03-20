385a0144e9d802fce0829ce102c0682a3cb0f4b5
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-03-20 16:54:09 +0530

    fix errors

--- front-end/pages/Profile/Add/Explorer/ExplorerMeat/ExplorerMeats.js
+++ front-end/pages/Profile/Add/Explorer/ExplorerMeat/ExplorerMeats.js
@@ -1,16 +1,15 @@
-// BeveragesPage.js
 import React from "react";
 import { View, Text } from "react-native";
 import ProductList from "../../../../../components/ProductList/ProductList";
 
-const BeveragesPage = () => {
+const ExploreMeat = () => {
   return (
     <View>
       <Text>Some content before ProductList</Text>
       {/* Use ProductList component */}
-      <ProductList category="beverages" />
+      <ProductList category="meat" />
     </View>
   );
 };
 
-export default BeveragesPage;
+export default ExploreMeat;
