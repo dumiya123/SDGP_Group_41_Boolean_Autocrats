b5fcf3ea5500451ed744a63e86cec9c273ba9887
Kaviindu <150445438+kaviiindu@users.noreply.github.com>
2024-03-22 06:15:31 +0530

    notiification-fornt-structure

--- front-end/pages/NotificationScreen/NotificationScreen.js
+++ front-end/pages/NotificationScreen/NotificationScreen.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import NotificationBox from "../../components/NotificationBox/NotificationBox";
+
+const ipAddress = "192.168.1.8";
 
 const NotificationScreen = () => {
+  const notificationsData = [
+    {
+      id: 1,
+      userId: 2,
+      readStatus: false,
+      notificationContent:
+        "Price of Carrot has increased from undefined to undefined",
+      createdAt: "2024-03-21",
+      updatedAt: "2024-03-21",
+    },
+    {
+      id: 2,
+      userId: 2,
+      readStatus: false,
+      notificationContent: "New message from John Doe",
+      createdAt: "2024-03-20",
+      updatedAt: "2024-03-20",
+    },
+    {
+      id: 3,
+      userId: 2,
+      readStatus: true,
+      notificationContent: "You have a meeting at 2:00 PM",
+      createdAt: "2024-03-19",
+      updatedAt: "2024-03-19",
+    },
+  ];
   return (
     <View>
-      <Text>Notification Screen</Text>
+      <NotificationBox notifications={notificationsData} />
     </View>
   );
 };
