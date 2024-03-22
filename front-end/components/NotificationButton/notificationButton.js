4a23e3b37a4f65b7fe2724219e8d32f362c08fba
Kavindu Hasaranga Yapa <150445438+kaviiindu@users.noreply.github.com>
2024-03-22 12:21:52 +0530

    Feature notification 2 (#157)
    * created-notification-end point
    
    * notiification-fornt-structure
    
    * make-notification-color-change

--- front-end/components/NotificationButton/notificationButton.js
+++ front-end/components/NotificationButton/notificationButton.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-
+import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import fetchNotifications from "../../pages/NotificationScreen/NotificationScreenFunctions";
 
 const NotificationButton = () => {
   const navigation = useNavigation();
-  
-  const handleNotificationPress = () => { 
-    navigation.navigate('Notifications');
+  const [unreadNotifications, setUnreadNotifications] = useState([]);
+
+  useEffect(() => {
+    const fetchAndSetNotifications = () => {
+      fetchNotifications()
+        .then((notifications) => {
+          const unread = notifications.filter(
+            (notification) => !notification.readStatus
+          );
+          setUnreadNotifications(unread);
+        })
+        .catch((error) => {
+          console.error("Error fetching notifications:", error);
+        });
+    };
+
+    fetchAndSetNotifications(); // Fetch notifications initially
+
+    const intervalId = setInterval(fetchAndSetNotifications, 5000); // Fetch notifications every 5 seconds
+
+    return () => clearInterval(intervalId); // Clean up interval on component unmount
+  }, [unreadNotifications]); // Re-fetch notifications when unreadNotifications change
+
+  const handleNotificationPress = () => {
+    navigation.navigate("Notifications");
   };
 
+  const buttonColor = unreadNotifications.length > 0 ? "red" : "white";
+
   return (
-    <Ionicons 
-      name="notifications" 
-      size={30} 
-      color="white" 
-      style={{ marginRight: 20 }} 
+    <Ionicons
+      name="notifications"
+      size={30}
+      color={buttonColor}
+      style={{ marginRight: 20 }}
       onPress={handleNotificationPress}
     />
   );
