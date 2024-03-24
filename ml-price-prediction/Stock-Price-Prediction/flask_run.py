19f102967a4f62866314a71541b678fd245a3d5b
Stealth_Black <159097453+StealthBlack99@users.noreply.github.com>
2024-03-24 23:12:38 +0530

    changed the api port

--- ml-price-prediction/Stock-Price-Prediction/flask_run.py
+++ ml-price-prediction/Stock-Price-Prediction/flask_run.py
@@ -24,4 +24,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True,port=6868)
